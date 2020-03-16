76e24aee00916f0105ad6142e4e37d3ddf47c758
pali08 <p.mikulaj@gmail.com>
2020-03-16 03:02:16 +0100

    Intro task

---
+++ load_all_files.py
@@ -0,0 +1,132 @@
+# /usr/bin/env python3
+import argparse
+import os
+import fileinput
+import xml.etree.ElementTree as etree
+import json
+from datetime import datetime
+from Bio.PDB.MMCIF2Dict import MMCIF2Dict
+from Bio.File import as_handle
+
+WATER_MOL_WEIGHT = 18.015
+
+
+def load_json(filename):
+    '''
+    :param filename: .json file as string
+    :return: dictionary with json data
+    '''
+    with open(filename) as js:
+        return json.load(js)
+
+
+def get_clashscore_from_xml(filename):
+    return etree.parse(filename).getroot()[0].get('clashscore')
+
+
+def get_pdbid_from_xml(filename):
+    return etree.parse(filename).getroot()[0].get('pdbid')
+
+
+def get_mmcif_high_resolution(filename):
+    print(filename)
+
+    def get_resolution(fnm):
+        try:
+            return MMCIF2Dict(fnm)['_reflns.d_resolution_high'][0]
+        except KeyError:
+            try:
+                return MMCIF2Dict(fnm)['em_3d_reconstruction.resolution'][0]
+            except KeyError:
+                return 'nan'
+
+    try:
+        return get_resolution(filename)
+    except UnicodeDecodeError:
+        with as_handle(filename, 'r', encoding='utf-16') as f:
+            return get_resolution(f)
+
+
+def get_orig_json_water_weight(filename):
+    print(filename)
+    dict_index = os.path.split(filename)[-1].rsplit('.', 1)[0]
+    js = load_json(filename)
+    for i in js[dict_index]:
+        if i['molecule_name'] == ['water']:
+            return "{0:.3f}".format(WATER_MOL_WEIGHT * i['number_of_copies'])
+    return 0
+
+
+def get_validated_json_model_count_filtered(filename):
+    print(filename)
+    try:
+        return len(load_json(filename)['Models'][0]['ModelNames'])
+    except IndexError:
+        return 'nan'
+
+
+def filepath_generator(path):
+    for root, dirs, files in os.walk(path):
+        dirs.sort()
+        files.sort()
+        for file in files:
+            yield os.path.join(root, file)
+
+
+def write_csv_column(column_list, file):
+    try:
+        if sum(1 for line in open(file)) != len(column_list):
+            return 'List length not equal to lengt of lines in file.'
+        for index, line in enumerate(fileinput.input(file, inplace=True)):
+            print(line.rstrip() + ';' + str(column_list[index]))
+    except FileNotFoundError:
+        with open(file, mode='w', encoding='utf-8') as f:
+            for i in column_list:
+                f.write(i + '\n')
+    return
+
+
+def read_file_get_list(path, column_name, required_data_function):
+    column_list = [column_name]
+    filename_generator = filepath_generator(path)
+    while True:
+        try:
+            column_list.append(required_data_function(next(filename_generator)))
+        except StopIteration:
+            break
+    return column_list
+
+
+def csv_name():
+    return 'output_' + datetime.now().strftime('%Y%m%d%H%M%S%f') + '.csv'
+
+
+def read_and_write(csv_file, columns, *datafolders):
+    if len(datafolders) != 4:
+        print(
+            'Incorrect number of datafolders in argument. Arguments must be: \
+            xml_folder, mmcif_folder, json folder, json_validated_folder')
+        return
+    if len(columns) != 4:
+        print('Incorrect number of items in column list')
+    data_functions = [get_clashscore_from_xml, get_mmcif_high_resolution, get_orig_json_water_weight,
+                      get_validated_json_model_count_filtered]
+    write_csv_column(read_file_get_list(datafolders[0], 'pdb_id', get_pdbid_from_xml), csv_file)
+    for i in range(0, len(datafolders)):
+        write_csv_column(read_file_get_list(datafolders[i], columns[i], data_functions[i]), csv_file)
+
+
+def main():
+    columns = ['clashscore', 'high_resolution', 'water_weight', 'model_count']
+    parser = argparse.ArgumentParser(description='Intro task - parse different types of the data.')
+    parser.add_argument('xml_files', help='Folder with xml files', type=str)
+    parser.add_argument('mmcif_files', help='Folder with mmcif files files', type=str)
+    parser.add_argument('json_files', help='Folder with original json files', type=str)
+    parser.add_argument('json_files_validation', help='Folder with results of validation.'
+                                                      'Files can be stored in subfolder', type=str)
+    args = parser.parse_args()
+    read_and_write(csv_name(), columns, args.xml_files, args.mmcif_files, args.json_files, args.json_files_validation)
+
+
+if __name__ == '__main__':
+    main()
