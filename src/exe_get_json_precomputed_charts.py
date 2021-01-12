f284f9ce466fee75a556afea4916cbecf0bedaf8
pali08 <p.mikulaj@gmail.com>
2021-01-12 23:56:03 +0100

    WIP: Json precomputed charts.

--- src/exe_get_json_precomputed_charts.py
+++ src/exe_get_json_precomputed_charts.py
@@ -1,4 +1,5 @@
 import csv
+import numpy as np
 
 
 def get_pairs_of_factors_from_autoplot_csv(filename):
@@ -38,7 +39,7 @@ def split_into_intervals(key, combined_pair_of_factors, number_of_intervals=None
     min_x = min(x_vals)
 
     if number_of_intervals is None:
-        number_of_intervals = int(max(x_vals)+1) - int(min(x_vals))
+        number_of_intervals = int(max(x_vals) + 1) - int(min(x_vals))
     print(number_of_intervals)
     factors_splitted_into_bins = [[] for i in range(0, number_of_intervals)]
     size_of_interval = (max_x - min_x) / number_of_intervals
@@ -50,3 +51,29 @@ def split_into_intervals(key, combined_pair_of_factors, number_of_intervals=None
         except IndexError:
             factors_splitted_into_bins[int(((i[0] - min_x) / size_of_interval) - 0.000001)].append(i)
     return key, factors_splitted_into_bins, min_x, max_x, min(y_vals), max(y_vals), size_of_interval
+
+
+def compute_data_for_interval(pairs_of_values_in_interval, minimum, interval_size, bucket_ordinal_number):
+    bucket = {
+                 "BucketOrdinalNumber": bucket_ordinal_number,
+                 "StructureCountInBucket": len(pairs_of_values_in_interval),
+                 "XfactorFrom": {
+                     "XfactorFromIsInfinity": "false",
+                     "XfactorFromOpenInterval": "false",
+                     "XfactorFromValue": "1976"
+                 },
+                 "XfactorTo": {
+                     "XfactorToIsInfinity": "false",
+                     "XfactorToOpenInterval": "true",
+                     "XfactorToValue": None
+                 },
+                 "YfactorAverage": None,
+                 "YfactorHighQuartile": None,
+                 "YfactorLowQuartile": None,
+                 "YfactorMaximum": None,
+                 "YfactorMedian": None,
+                 "YfactorMinimum": None
+             },
+    y_factor = [i[1] for i in pairs_of_values_in_interval]
+    return np.average(y_factor), np.quantile(y_factor, 0.75), np.quantile(y_factor, 0.75), np.max(y_factor), np.median(
+        y_factor), np.min(y_factor)
